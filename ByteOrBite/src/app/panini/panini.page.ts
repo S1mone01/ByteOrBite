e8b84122b667352450e17955a9e91c4f854e7725
S1mone01 <simonesaluto@gmail.com>
2026-08-10 10:55:39 +0200

    Feat: Nascondi ingredienti non disponibili dalle aggiunte extra

--- ByteOrBite/src/app/panini/panini.page.ts
+++ ByteOrBite/src/app/panini/panini.page.ts
@@ -131,7 +131,7 @@ export class PaniniPage implements OnInit, OnDestroy {
       .map(ing => ({ ...ing, isBase: true, checked: true }));
 
     this.ingredientiExtra = this.allIngredienti
-      .filter(ing => !baseIds.includes(ing.id))
+      .filter(ing => !baseIds.includes(ing.id) && ing.disponibile != 0)
       .map(ing => ({ ...ing, isBase: false, checked: false }));
 
     this.isModalOpen = true;
